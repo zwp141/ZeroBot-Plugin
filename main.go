28cdc6473543d5f81186eef19b7ad014a4e8c6d4
zwp141 <152165297+zwp141@users.noreply.github.com>
2024-04-09 21:37:59 +0800

    Update main.go

--- main.go
+++ main.go
@@ -224,7 +224,7 @@ func init() {
 	// 直接写死 AccessToken 时，请更改下面第二个参数
 	token := flag.String("t", "", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://127.0.0.1:8080", "Set Url of WSClient.")
+	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
 	adana := flag.String("n", "测试", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
